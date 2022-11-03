d6bdeba5dcfb46343ab363f556e66924b5c47d10
JoepLiefting <joep.liefting@gmail.com>
2022-11-03 14:00:10 +0100

    DiGraph edges removed

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+import networkx as nx
 import numpy as np
 import read_files
+from networkx import DiGraph, from_numpy_matrix, relabel_nodes, set_node_attributes
+
+DISTANCES = read_files.D[100]
+
+A = np.array(DISTANCES, dtype=[("cost", int)])
+G = from_numpy_matrix(A, create_using=nx.DiGraph())
+G.remove_edges_from(read_files.no_route_truck)
+
+print(G)
